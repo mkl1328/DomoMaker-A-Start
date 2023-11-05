96133f5d2d7d6ea886adc3bf457e87eb3f486f77
Max Lama <mkl1328@rit.edu>
2023-11-05 00:21:35 -0400

    Up to and step 4 DMB

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -6,7 +6,10 @@ const loginPage = (req, res) => res.render('login');
 
 const signupPage = (req, res) => res.render('signup');
 
-const logout = (req, res) => res.redirect('/');
+const logout = (req, res) => {
+  res.session.destroy();
+  res.redirect('/');
+}
 
 const login = (req, res) => {
   const username = `${req.body.username}`;
